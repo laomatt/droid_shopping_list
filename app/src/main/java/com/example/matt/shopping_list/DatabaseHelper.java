da8921af09de1840eb887756fc8320adc1d4a240
Matt L <matt@Matts-MacBook-Pro-2.local>
2017-07-02 17:34:20 -0700

    first commit

---
+++ app/src/main/java/com/example/matt/shopping_list/DatabaseHelper.java
@@ -0,0 +1,58 @@
+package com.example.matt.shopping_list;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.HashMap;
+
+/**
+ * Created by matt on 7/2/17.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public final static String DATABASE_NAME="list.db";
+    public final static String TABLE_NAME="list_items";
+    public final static String COL_1="ID";
+    public final static String COL_2="Content";
+    public final static String COL_3="Completed";
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+       // SQLiteDatabase db = this.getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, Content TEXT, Completed BOOLEAN)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    public boolean insertData(String content) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValue = new ContentValues();
+
+        contentValue.put(COL_2, content);
+        contentValue.put(COL_3, false);
+        long result = db.insert(TABLE_NAME, null, contentValue);
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor result = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+
+        return result;
+    }
+}
---
+++ app/src/main/java/com/example/matt/shopping_list/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.matt.shopping_list;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    DatabaseHelper myDb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myDb = new DatabaseHelper(this);
+        
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Cursor res = myDb.getAllData();
+        StringBuffer buffer = new StringBuffer();
+
+        TextView list = (TextView) findViewById(R.id.item_list);
+        if (res.getCount() != 0) {
+            while (res.moveToNext()) {
+                buffer.append("ID: " + res.getString(0) + "\n");
+                buffer.append("WHat : " + res.getString(1) + "\n");
+                buffer.append("done : " + res.getString(2) + "\n");
+            }
+
+            list.setText(buffer);
+        } else {
+            list.setText("nothing to show");
+            
+        }
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.input_text);
+        String message = editText.getText().toString();
+
+        // make the insert
+        boolean success = myDb.insertData(message);
+
+        TextView err_mess = (TextView) findViewById(R.id.status_message);
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        if (success) {
+            Toast.makeText(MainActivity.this,"inserted",Toast.LENGTH_LONG).show();
+            // startActivity(intent);
+        } else {
+            Toast.makeText(MainActivity.this,"error",Toast.LENGTH_LONG).show();
+            err_mess.setText("There was an error");
+        }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
